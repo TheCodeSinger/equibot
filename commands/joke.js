1359111443609e9073a079f42e9edfc58755c822
David Close <david@cohesity.com>
2020-05-23 02:34:49 -0700

    Added knock knock jokes and two-liners

--- commands/joke.js
+++ commands/joke.js
@@ -1,9 +1,4 @@
 /**
- * TODO:
- *   1. Add support for interactive joke types, like knock-knock.
- */
-
-/**
  * Displays a random joke.
  *
  * INSTALLATION:
@@ -12,18 +7,23 @@
  *
  * @example   !joke
  * @example   !joke oneliner
+ * @example   !joke kk
  */
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
-    const specifiedType = (args[0] || '').toLowerCase();
+    let specifiedType = (args[0] || '').toLowerCase();
     let jokes;
 
     if (specifiedType) {
+      // Reassign specifiedType to the proper type name.
+      specifiedType = client.jokeAliases.get(specifiedType) || specifiedType;
+
       // Find a matching joke type from the list.
-      jokes = client.jokes.get(specifiedType) || client.jokes.get(client.jokeAliases.get(specifiedType));
+      jokes = client.jokes.get(specifiedType);
     } else {
       // No type specified. Select a random type.
-      jokes = client.jokes.get(client.getRandomItem(client.jokeTypes));
+      specifiedType = client.getRandomItem(client.jokeTypes);
+      jokes = client.jokes.get(specifiedType);
     }
 
     if (!jokes) {
@@ -31,14 +31,109 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
       return message.reply(`I don't know any jokes of that variety. Send your suggestions using \`idea\` command.`);
     }
 
-    const oneLinerEmbed = {
-      embed: {
-        color: client.config.colors.default,
-        description: client.getRandomItem(jokes),
-      }
-    };
+    /**
+     * Share a knock knock joke with the initiator.
+     *
+     * @param   {Array}   joke   The unique parts of a knock knock joke.
+     */
+    function knockKnock(joke) {
+      const theOpening = "Knock knock!";
+      const whosThere = 'Who\'s there?';
+      const theName = joke[0];
+      const theSetup = theName + ' who?';
+      const thePunchline = joke[1];
 
-    message.channel.send(oneLinerEmbed);
+      /**
+       * Scrubs a string by removing quotes, adding question mark at the end,
+       * and converting to all lowercase for better comparisons.
+       *
+       * @param   {String}   question   A string to scrub.
+       * @return  {String}   The scrubbed string.
+       */
+      function scrubQuestion(question) {
+        if (!question.includes('?')) {
+          question = question + '?';
+        }
+        return question.replace('\'', '').toLowerCase();
+      }
+
+      // Send The Opening.
+      message.channel.send(theOpening).then(() => {
+        // Wait for the "Who's There?".
+        message.channel
+          .awaitMessages(response => (
+            response.author === message.author &&
+            scrubQuestion(response.content) === scrubQuestion(whosThere)
+            ), { max: 1, time: 10000, errors: ['time'] })
+          .then(() => {
+            // Send The Name.
+            message.channel.send(theName).then(() => {
+              // Wait for the "NAME who?".
+              message.channel
+                .awaitMessages(response => (
+                  response.author === message.author &&
+                  scrubQuestion(response.content) === scrubQuestion(theSetup)
+                ), { max: 1, time: 10000, errors: ['time'] })
+                .then(() => {
+                  // Send The Punchline.
+                  message.channel.send(thePunchline);
+                })
+                .catch(() => {
+                  message.channel.send(`You were supposed to answer "${theSetup}", but I'm done waiting. If you want to hear a joke, ask me again.`);
+                });
+            });
+          })
+          .catch(() => {
+            message.channel.send(`You were supposed to answer "${whosThere}", but I'm done waiting. If you want to hear a joke, ask me again.`);
+          });
+      });
+    }
+
+    /**
+     * Get a Discord embed for a one-liner joke.
+     *
+     * @param   {String}   joke   The text of the joke.
+     */
+    function getOneLinerEmbed(joke) {
+      return {
+        embed: {
+          color: client.config.colors.default,
+          description: joke,
+        }
+      }
+    }
+
+    /**
+     * Get a Discord embed for a two-liner joke.
+     *
+     * @param   {String[]}   joke   The parts of the joke.
+     */
+    function getTwoLinerEmbed(joke) {
+      return {
+        embed: {
+          color: client.config.colors.default,
+          title: joke[0],
+          description: joke[1],
+        }
+      }
+    }
+
+    // Select a random joke and send the message.
+    var joke = client.getRandomItem(jokes);
+    switch(specifiedType) {
+      case 'knockknock':
+        knockKnock(joke);
+        break;
+      case 'twoliners':
+        message.channel.send(getTwoLinerEmbed(joke));
+        break;
+      case 'oneliners':
+        message.channel.send(getOneLinerEmbed(joke));
+        break;
+      default:
+        message.channel.send('Sorry.. I.. can\'t think of a good joke of that type.');
+        client.logger.error(`Error finding 'joke' of type: ${specifiedType}`);
+    }
   } catch (e) {
     client.logger.error(`Error executing 'joke' command: ${e}`);
   }
@@ -55,6 +150,6 @@ exports.help = {
   name: 'joke',
   category: 'Fun',
   description: 'Displays a random joke.',
-  detailedDescription: 'Displays a random joke. You can optionally specify a type, like one-liner or knock-knock. For example: \'!joke kk\' or \'!joke 1l\'. Honestly, though, I don\'t know any knock-knock jokes yet.',
+  detailedDescription: 'Displays a random joke. You can optionally specify a type, like one-liner or knock-knock. For example: \'!joke kk\' or  \'!joke knock\' or \'!joke 1l\' or \'!joke one\'.\n\nPlease submit your favorite jokes and recommendations for additional types of jokes.',
   usage: 'joke [type]',
 };
