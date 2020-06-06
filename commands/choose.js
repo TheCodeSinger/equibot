1315c4ab07924873628642767c762880c154e04c
David Close <david@cohesity.com>
2020-06-06 00:23:20 -0700

    Cmd Choose: Allow comma separated lists.

--- commands/choose.js
+++ commands/choose.js
@@ -1,24 +1,38 @@
 const moment = require("moment");
 
 /**
- * Randomly chooses one of the provided options.
+ * Randomly chooses one of the provided options separated by commas, otherwise
+ * spaces.
  *
  * @example   !choose eq1 eq2
- * @example   !choose Peter Hook Tinkerbell
+ * @example   !choose Peterpan, Captain Hook, Tinkerbell
  */
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   try {
+    const argString = args.join(' ');
+    let selections = args;
+    let selected = '';
+
     if (args.length < 2) {
       return message.reply('Provide at least two options and I will select one at random.');
     }
+    if (argString.indexOf(',') > -1) {
+      // List is comma-delimited.
+      selections = argString.split(',');
+      selection = selections[Math.floor(Math.random() * selections.length)];
+    }
+    client.logger.debug(`selections: ${JSON.stringify(selections)}`);
 
-    const selectedIndex = Math.floor(Math.random() * args.length);
-    client.logger.debug(`selectedIndex: ${selectedIndex}`);
+    selected = selections[Math.floor(Math.random() * selections.length)];
+    client.logger.debug(`selected: ${selected}`);
 
     const embed = {
       embed: {
         color: client.config.colors.default,
-        description: `I choose "${args[selectedIndex]}"`,
+        description: `I choose "${selected.trim()}"`,
+        footer: {
+          text: `The choices were ${argString}`,
+        }
       }
     };
 
@@ -39,6 +53,6 @@ exports.help = {
   name: 'choose',
   category: 'Fun',
   description: 'Chooses one of provided options.',
-  detailedDescription: 'Chooses at random one of provided options. Each option must be a single word.',
-  usage: 'choose <optionOne> <optionTwo> <optionThree> ...',
+  detailedDescription: 'Chooses at random one of provided options. If any option is more than one word, you must use commas to separate all options. Otherwise, you can simply use spaces to separate single-word options.',
+  usage: 'choose <option one>, <option two>, <option three>, ...',
 };
