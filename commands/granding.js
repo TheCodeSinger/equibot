9fb69449b5bec472bef3b170be05c716af7e1861
David Close <david@cohesity.com>
2020-06-30 14:21:58 -0700

    Cmd: Granding - Added the command.

---
+++ commands/granding.js
@@ -0,0 +1,28 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  try {
+    const output = {
+      'embed': {
+        'color': client.config.colors.default,
+        'author': {
+          'name': 'verb'
+        },
+        'title': 'Granding (grand′ ĭng)',
+        'description': 'The act of starting a lotto and then promptly forgetting you started a lotto.'
+      }
+    };
+    message.channel.send(output);
+  } catch (e) {
+    client.logger.error(`Error executing 'granding' command: ${e}`);
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'User',
+};
+
+exports.help = {
+  name: 'granding',
+};
