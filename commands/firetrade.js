34d148ca7bf4d19cd52cfad9dfecfc095e660e6a
kastang <josh.kastang@gmail.com>
2020-09-06 10:18:59 -0400

    refactored firetrader.js to support tce and multiple eq traders

--- commands/firetrade.js
+++ commands/firetrade.js
@@ -1,109 +1,162 @@
-const moment = require("moment");
+const https = require("https")
+const async = require('async');
 
 /**
- * Displays Fire Trader's price for an item.
+ * Displays item buy prices for EQ traders.
  *
- * @example   !firetrading <item name>
+ * @example   !tr[ade] <item name>
  */
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const itemList = client.tornData.items;
-  const lordofFire = client.quotedMembers.get('lordoffire');
-  const timestamp = moment().format();
+  const item_list_cache = client.tornData.items;
+  const traders = client.config.params.firetrade.traders;
+  const price_url = client.config.params.firetrade.price_url;
 
-  /**
-   * Returns an embed object with Fire Trader's pricing for an item.
-   *
-   * @param   {Object}   item   The Torn item object.
-   * @param   {String}   id     The Torn item ID.
-   * @return  {Object}   A Discord embed object.
-   */
-  function embedFireTrading(item, id) {
-    return {
-      'embed': {
-        'color': client.config.colors.lof,
-        'title': 'LordofFire\'s price:',
-        'description': '[Start Trade](https://www.torn.com/trade.php#step=start&userID=2411517/) ' +
-          '| [Pricelist](https://tinyurl.com/fire-trading-pricelist/)',
-        'thumbnail': {
-          'url': 'https://alltornup.netlify.com/TornItems965/' + id + '.png'
-        },
-        'author': {
-          'name': item.name + ' #' + id
-        },
-        'image': {
-          'url': 'https://arsonwarehouse.com/!/lordoffire/bids/' + id + '@2x.png?timestamp=' + timestamp
-        },
-        'footer': {
-          'text': 'Fire Trading'
-        }
-      }
-    };
-  }
+  var trader_urls = [];
+  Object.keys(traders).forEach(trader => {
+    trader_urls.push(`${price_url}/${trader}`)
+  })
 
-  try {
-    if (!args[0]) {
-      // No item specified. Display a random LoF quote.
+  // process !tr requests where the normalized item name is found
+  async function process(item_name) {
+    var formatted_desc = ''
+
+    // process each trader_urls as a promise to get the api response
+    const results = await Promise.all(trader_urls.map((url) => fetch(url).then((r) => r.json()))).catch((error) => {});
+    if(results == undefined) {
+      // if the promise fails (most commonly an api error, note it and move stop processing
       return message.channel.send({
         embed: {
-          color: client.config.colors.default,
-          author: {
-            name: 'Fire Trading',
-            url: 'https://tinyurl.com/firetrading/',
-          },
-          description: client.getRandomItem(lordofFire.quotes),
+          description : 'API returned an unexpected error. Try again in a few minutes.',
         }
       });
     }
 
-    let filtered = client.filterItems(itemList, args[0]);
-    let itemKeys = Object.keys(filtered);
-
-    if (!itemKeys.length) {
-      return message.reply('No item found!');
-    }
-
-    if (itemKeys.length > 9) {
-      return message.reply('More than 9 items found. Be more precise, please!');
-    }
-
-    if (itemKeys.length > 1) {
-      const content = itemKeys.length + ' items found. Choose by answering with the displayed number!';
-      let choices = '';
-      itemKeys.forEach(id => choices += '[' + id + '] ' + itemList[id].name + '\n');
-
-      return message.reply(content + '```' + choices + '```')
-        .then(() => {
-          message.channel
-            .awaitMessages(response =>
-              (response.author.username === message.author.username && itemKeys.includes(response.content)),
-              {
-                max: 1,
-                time: 10000, // 10 seconds
-                errors: ['time'],
-              }
-            )
-            .then((collected) => {
-              const selectedId = collected.first().content;
-              message.channel.send(embedFireTrading(itemList[selectedId], selectedId));
-            })
-            .catch(() => {
-              message.reply('Done waiting. You can ask me again if you still need the info.');
-            });
+    results.some(result => {
+      // an error response is expected if the endpoint is unable to build the proper payload
+      if('error' in result) {
+        message.channel.send({
+          embed : {
+            description : 'API returned an expected error. Try again in a few minutes.',
+          }
         });
+        return true
+      }
+      
+      trader = Object.keys(result)[0]
+      items = result[trader];
+      if (item_name in items) {
+          item = items[item_name];
+
+          let price = item['price'];
+          let trade_url = traders[trader]['trade_url'];
+          let price_url = traders[trader]['price_url'];
+          let forum_url = traders[trader]['forum_url'];
+
+          formatted_desc += `
+          **${trader}: ${price}**
+          [Start Trade](${trade_url}) | [Pricelist](${price_url}) | [Forum](${forum_url})
+          `;
+        }
+    });
+
+    message.channel.send({
+      embed: {
+        title: `**Prices for ${item_name}:**`,
+        description: formatted_desc,
+        thumbnail: {
+          url: item['img']
+        },
+        footer: {
+          text: 'Please upvote your trader on the Pricelist and Forums'
+        }
+      }
+    });
+  }
+
+  // !tr help
+  if(args.length == 0 || (args.length == 1 && args[0] == 'help')) {
+    return message.channel.send({
+      embed: {
+        description: `
+          Usage: \`!tr <Item Name>\`\n
+          Examples:
+          \`!tr Xanax\`
+          \`!tr Big Box of Chocolate Bars\`
+        `
+      }
+    });
+  }
+
+  // !tr <Item Name>
+  let found_match = false;
+  let partial_matches = [];
+  let item_name = args.join(' ');
+  Object.keys(item_list_cache).some(function(item_id, index) {
+    let normalized_item_name = item_list_cache[item_id]['name']
+
+    // if an exact match is found
+    // e.g. `!tr xanex`
+    if(item_name.toUpperCase() == normalized_item_name.toUpperCase()) {
+      found_match = true
+      process(normalized_item_name)
+      return true
     }
 
-    // Exactly one match found. Show it.
-    message.channel.send(embedFireTrading(itemList[itemKeys[0]], itemKeys[0]));
+    // keep track of partial matches
+    if(normalized_item_name.toUpperCase().startsWith(item_name.toUpperCase())) {
+      partial_matches.push(normalized_item_name);
+    }
+  });
 
-  } catch (e) {
-    client.logger.error(`Error executing 'firetrading' command: ${e}`);
+  // if a single partial match is found
+  // e.g. `!tr xan`
+  if(!found_match && partial_matches.length == 1) {
+    found_match = true;
+    message.channel.send({
+      embed: {
+        description: `
+          Partial matches found for "${item_name}, assuming the following:
+          \`!tr ${partial_matches[0]}\`
+          `
+      }
+    })
+    process(partial_matches[0])
+  }
+
+  // if one or more partial matches are found
+  // e.g. `!tr ford`
+  if(!found_match && partial_matches.length > 1) {
+    found_match = true;
+    let matches_string = '';
+    partial_matches.forEach(match => {
+      matches_string += `\`!tr ${match}\`\n`
+    });
+
+    return message.channel.send({
+      embed: {
+        description: `
+          Partial matches found for "${item_name}, use one of the below commands:
+          ${matches_string}
+          `
+      }
+    })
+  }
+
+  // default case if an exact or partial match cannot be found
+  // e.g. `!tr fake item`
+  if(!found_match) {
+    return message.channel.send({
+      embed: {
+        description: `Could not find partial or exact match for "${item_name}". Please check your spelling and try again.`
+      }
+    });
   }
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['ft', 'fire'],
+  aliases: ['tr', 'trade'],
   permLevel: 'User',
 };
 
