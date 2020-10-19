1343ff2394f5cdc44ba5553dd61ee4e24cdf2b18
David Close <david@cohesity.com>
2020-10-18 21:50:29 -0700

    Cmd idea: Send output to #api-dev channel and all developers.

--- commands/idea.js
+++ commands/idea.js
@@ -9,9 +9,28 @@
       message.channel.send('You must provide a message to pass on to the developer.');
       return;
     }
-    // Get the Developer's User object: Aarlo#2177
-    let developer = message.guild.member('550079162564476997');
-    developer.send(`${message.author.toString()} sends the following message: ${args.join(' ')}`);
+
+    const idea = `${message.author.toString()} sends the following message: ${args.join(' ')}`;
+
+    /**
+     * Send DM to developers.
+     *   Aarlo: 550079162564476997
+     *   kastang: 576918909672751124
+     *   Orion: 320016515602579456
+     */
+    const developerIds = ['550079162564476997', '576918909672751124', '320016515602579456'];
+    developerIds.forEach(id => {
+      const developer = message.guild.member(id);
+      developer.send(idea);
+    });
+
+    /**
+     * Send to a channel.
+     *   #api-dev: 680114562057502771
+     */
+    const channel = client.channels.cache.find(channel => channel.id === '680114562057502771');
+    channel.send(idea);
+
     message.reply('Message sent. Thank you for your feedback!');
   } catch (e) {
     client.logger.error(`Error executing 'idea' command: ${e}`);
