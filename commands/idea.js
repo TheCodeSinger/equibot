35ccc4349fa19df3ca03a02d36af64ecf10516f6
David Close <david@cohesity.com>
2020-10-18 22:11:34 -0700

    Cmd idea: Send output to #api-dev channel and all developers. part 2

--- commands/idea.js
+++ commands/idea.js
@@ -20,16 +20,30 @@
      */
     const developerIds = ['550079162564476997', '576918909672751124', '320016515602579456'];
     developerIds.forEach(id => {
-      const developer = message.guild.member(id);
-      developer.send(idea);
+      const guild = message.guild;
+      if (guild) {
+        const developer = guild.member(id);
+        if (developer) {
+          developer.send(idea);
+        } else {
+          client.logger.debug(`No member found with ID ${id}.`);
+        }
+      } else {
+        client.logger.debug(`No guilds found. Must have been a DM.`);
+      }
     });
 
     /**
      * Send to a channel.
      *   #api-dev: 680114562057502771
      */
-    const channel = client.channels.cache.find(channel => channel.id === '680114562057502771');
-    channel.send(idea);
+    const devChannelId = '680114562057502771';
+    const channel = client.channels.cache.find(channel => channel.id === devChannelId);
+    if (channel) {
+      channel.send(idea);
+    } else {
+      client.logger.debug(`No channel found with ID ${devChannelId}.`);
+    }
 
     message.reply('Message sent. Thank you for your feedback!');
   } catch (e) {
@@ -47,6 +61,6 @@ exports.conf = {
 exports.help = {
   name: 'idea',
   category: 'System',
-  description: 'Sends message to bot developer.',
+  description: 'Sends message to bot developers.',
   usage: 'idea <description of idea, bug, suggestion, or comment>'
 };
