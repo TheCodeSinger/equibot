28c675d4a4a07d9659d791b94a9f850d63ceb0c2
kastang <josh.kastang@gmail.com>
2021-01-25 18:37:01 -0500

    Direct Message lotto server invite links to users.

---
+++ commands/servers.js
@@ -0,0 +1,30 @@
+/**
+ * Direct Message lotto server invite links to users.
+ *
+ * @example   !servers
+ */
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+    const discord_servers = client.config.params.discord_servers;
+    
+    // build the body of the message
+    let body = '';
+    body += discord_servers.join('\n');
+    body += '\n\n**Please report any broken or missing invites to `@API Developer`**';
+    
+    // message the user directly
+    message.author.send(body);
+};
+  
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 'User',
+};
+
+exports.help = {
+    name: 'servers',
+    category: 'Torn',
+    description: 'Direct Message lotto server invite links to users.',
+    usage: 'servers',
+};
