4650db93e3c662823ad81cbac6ab5c65bc3241fb
David Close <david@cohesity.com>
2021-10-01 14:05:25 -0700

    enable blaking command

--- commands/blaking.js
+++ commands/blaking.js
@@ -17,7 +17,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 };
 
 exports.conf = {
-  enabled: false,
+  enabled: true,
   guildOnly: false,
   aliases: ['blakey'],
   permLevel: 'User',
